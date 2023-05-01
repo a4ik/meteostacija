6b20b26015cb54f3c9a99bb85612cea2602e2e6d
a4ik <132303425+a4ik@users.noreply.github.com>
2023-05-01 23:31:21 +0300

    Add files via upload

---
+++ sketch_apr28a.ino
@@ -0,0 +1,122 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <DHT.h>
+//preferences url http://arduino.esp8266.com/stable/package_esp8266com_index.json
+//https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+const char *ssid = "MK";
+const char *password = "29528242";
+const char* host = "192.168.0.179";
+
+DHT dht(26, DHT11);
+
+
+
+void setup(void) {
+
+  Serial.begin(115200);
+  dht.begin();
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+ 
+}
+
+void loop(void) {
+ float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();  
+  if(isnan(temperature) || isnan(humidity)){
+    Serial.println("Failed to read DHT11");
+  }else{
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" *C");
+    delay(3000);
+  }
+   
+    Serial.print("connecting to ");
+    Serial.println(host);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+
+ 
+
+delay(60000);
+    // This will send the request to the server
+ client.print(String("GET http://192.168.0.179/project/connect.php?") + 
+                          ("&temperature=") + temperature +
+                          ("&humidity=") + humidity +
+                          " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 1000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+        
+    }
+
+    Serial.println();
+    Serial.println("closing connection");
+}
+
+
+/*float readDHTTemperature() {
+  // Sensor readings may also be up to 2 seconds
+  // Read temperature as Celsius (the default)
+  float temperature = dht.readTemperature();
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  else {
+    Serial.println(t);
+    return t;
+  }
+}
+
+float readDHTHumidity() {
+  // Sensor readings may also be up to 2 seconds
+  float humidity = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  else {
+    Serial.println(h);
+    return h;
+  }
+}*/
